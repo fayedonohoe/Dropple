782dc0ed65f80657ffecce53ff22aad3f76e97ce
Faye Donohoe <fayedonohoe@hotmail.com>
2018-12-20 00:40:12 +0000

    Big Update

--- js/game.js
+++ js/game.js
@@ -1,17 +1,3 @@
-//class mainMenu extends Phaser.Scene{
-//    
-//    constructor ()
-//    {
-//        super('Menu');
-//
-//        this.active;
-//        this.currentScene;
-//        
-//        this.menubg;
-//    }
-
-
-
 let mainMenu = new Phaser.Scene('Menu')
     
 
@@ -21,25 +7,31 @@ mainMenu.preload = function() {
         this.load.image('helpbutton', 'assets/helpbutton.png');
         this.load.image('mutebutton', 'assets/mutebutton.png');
         
-        this.load.audio("menu_music", 'assets/Komiku_Home.mp3')
+        this.load.audio("menu_music", 'assets/Komiku_Home.mp3');
+        this.load.audio("game_music", 'assets/Komiku_Skate.mp3')
+    
     }
     
 mainMenu.create = function(){
+    
+        window.addEventListener('resize', resize);
+        resize();
         
         let menubg = this.add.sprite(0, 0, 'menubg');
-        let playbutton = this.add.sprite(60, 250, 'playbutton');
-        let helpbutton = this.add.sprite(60, 330, 'helpbutton');
-        let mutebutton = this.add.sprite(60, 410, 'mutebutton');
+        let playbutton = this.add.sprite(120, 650, 'playbutton');
+        let helpbutton = this.add.sprite(120, 850, 'helpbutton');
+        let mutebutton = this.add.sprite(120, 1050, 'mutebutton');
         
         var menu_music = this.sound.add('menu_music');
         var game_music = this.sound.add('game_music');
+    
+        menu_music.stop(); //does nothing
         
         if(!mute){
             menu_music.play();
         }
         
         menubg.setOrigin(0,0);
-        this.menubg.setScale(4);
         playbutton.setOrigin(0,0);
         helpbutton.setOrigin(0,0);
         mutebutton.setOrigin(0,0);
@@ -49,8 +41,9 @@ mainMenu.create = function(){
         playbutton.on('pointerdown', () => menu_music.stop());   
         
         helpbutton.setInteractive();
+        playbutton.on('pointerdown', () => menu_music.stop());
         helpbutton.on('pointerdown', () => this.scene.start(helpScreen));  
-        playbutton.on('pointerdown', () => menu_music.stop()); 
+         
         
         mutebutton.setInteractive();
         //mutebutton.on('pointerdown', () => mute=true);
@@ -66,49 +59,94 @@ mainMenu.create = function(){
 var helpScreen = new Phaser.Scene('Help');
 
 helpScreen.preload = function(){
-    this.load.image('helpbg', 'assets/bg_menu.jpg');
+    this.load.image('helpbg', 'assets/bg_help.jpg');
     this.load.image('menubutton', 'assets/menubutton.png')
     
 }
 
 helpScreen.create = function () {
-    
-
-    
+        
     let helpbg = this.add.sprite(0, 0, 'helpbg');
-    this.helpbg.setScale(4);
-    let menubutton = this.add.sprite(60, 250, 'menubutton');
+    let menubutton = this.add.sprite(140, 1300, 'menubutton');
     
     helpbg.setOrigin(0,0);
     menubutton.setOrigin(0,0);
     
     menubutton.setInteractive();
-    //menubutton.on('pointerdown', () => this.menu_music.stop());
+    //menubutton.on('pointerdown', () => menu_music.stop());
     menubutton.on('pointerdown', () => this.scene.start(mainMenu));
-    
-    
-    
+   
 }
 
 
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
-let counter = 99;
 let total = 0;
-let mute = false;
+var mute = false;
 
 
 // parameters for our scene
 gameScene.init = function () {
-    this.playerSpeed = 5;
+    this.playerSpeed = 10;
 }
 
 // load assets
 gameScene.preload = function () {
+    
+    //Loading Screen Preload
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(340, 870, 320, 80);
+    
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var loadingText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 40,
+        text: 'Taking Off...',
+        style: {
+            font: '40px calibri',
+            fill: '#ffffff'
+        }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+        x: width / 2 + 15,
+        y: height / 2 + 35,
+        text: '0%',
+        style: {
+            font: '22px calibri',
+            fill: '#ffffff'
+        }
+    });
+    percentText.setOrigin(0.5, 0.5);
+    
+    this.load.on('progress', function (value) {
+        //console.log(value);
+        progressBar.clear();
+        progressBar.fillStyle(0xffffff, 1);
+        progressBar.fillRect(350, 880, 300 * value, 60);
+        percentText.setText(parseInt(value * 100) + '%');
+    });    
+    
+    this.load.on('complete', function () {
+        //console.log('complete');
+        progressBar.destroy();
+        progressBox.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+    });
+    
     // load images
     this.load.image('background', 'assets/bg_blue.jpg');
     this.load.image('player', 'assets/abiUP.png');
     this.load.image('drop', 'assets/drop.png');
+    
+    this.load.image('black', 'assets/bg_black.jpg');
+    this.load.image('playagainbutton', 'assets/playagain.png')
+    this.load.image('backtomenubutton', 'assets/menubutton.png')
     
     this.load.audio("game_music", 'assets/Komiku_Skate.mp3')
 };
@@ -121,10 +159,15 @@ gameScene.create = function () {
     if(!mute){
         let game_music = this.sound.add('game_music');
         game_music.play();
+        game_music.setLoop(true);
     }
 
     
-    var timer = this.time.delayedCall(30000, function() {gameScene.gameOver()}, [], this);
+//    var timer = this.time.delayedCall(30000, function() {gameScene.gameOver()}, [], this);
+//    console.log(timer);
+    
+    // FOR TESTING ONLY
+    var timer = this.time.delayedCall(1000, function() {gameScene.gameOver()}, [], this);
     console.log(timer);
         
     
@@ -132,24 +175,23 @@ gameScene.create = function () {
     this.counter =100;
     
     // create bg sprite
-    let bg = this.add.sprite(0, 0, 'background');
+    let bg = this.add.sprite(-50, 0, 'background');
     // change the origin to the top-left corner
     bg.setOrigin(0, 0);
-    this.bg.setScale(4);
+    //this.bg.setScale(4);
 
     // create the player
-    this.player = this.physics.add.sprite(80, this.sys.game.config.height - 100, 'player');
+    this.player = this.physics.add.sprite(80, this.sys.game.config.height - 280, 'player');
     this.player.body.allowGravity = false;
 
     //reducing the width and height 
-    this.player.setScale(1.2);
+    this.player.setScale(2);
     
     drops = this.physics.add.group({
         key: 'drop',
-        repeat: 99,
+        repeat: 149,
         setXY: { x: Phaser.Math.Between(0, innerWidth-20), 
                  y: 30,
-                 //stepX: 20,
                  stepY: -30
                }
     });
@@ -161,7 +203,9 @@ gameScene.create = function () {
     //player is alive
     this.isPlayerAlive = true;
     
-    totalText = this.add.text(140, 30, '', { fontFamily: 'Arial Rounded', fontSize: '28px', fill: '#000' });
+    totalText = this.add.text(320, 80, '', { fontFamily: 'Calibri', fontStyle:'bold', fontSize: '90px', fill: '#000' });
+    
+    
     
 }; // end create
 
@@ -190,10 +234,17 @@ gameScene.update = function () {
       } 
     }
     
-    console.log(total);
+    //console.log(total);
 }; // end update
 
+
+function shutUp(){
+        //this.menu_music.stop();
+        game_music.stop();
+    }
 //////////////////////////// FUNCTIONS ////////////////////////////////
+
+
 
 function toggleMute(){
     if(!mute){
@@ -201,7 +252,7 @@ function toggleMute(){
     }
     else{
         mute = false;
-        if(Menu.isActive){
+        if(mainMenu.isActive){
             menu.menu_music.play();
         }
     }
@@ -209,7 +260,7 @@ function toggleMute(){
 
 function collectDrop(player, drop)
 {
-    console.log("collect drop");
+    //console.log("collect drop");
     drop.disableBody(true, true);
     drop.enableBody(true, drop.x, 0, true, true);
     resetDrop(drop);
@@ -223,11 +274,12 @@ function resetDrop(drop){
     drop.speed = drop.speed*1.1;
     drop.gravity = drop.gravity*2;
     drop.y = Math.random() * -400;
-    drop.x = Math.random() *400;
+    drop.x = Math.random() * 950;
 }
 
 // end the game
 gameScene.gameOver = function () {
+    
 
     //player alive flag set to dead
     this.isPlayerAlive = false;
@@ -237,25 +289,50 @@ gameScene.gameOver = function () {
     
     //this.game_music.stop();
     mute = true;
-    
-    //fading out
-    this.time.delayedCall(250, function () {
-        this.cameras.main.fade(250);
-    }, [], this);
-    
-    
-    
-    
-    
 
-    //restart game
-    this.time.delayedCall(10000, function () {    
-        this.scene.restart();
-        mutr = false;
-    }, [], this);
+    
+    let black = this.add.sprite(0, 0, 'black');
+    black.setOrigin(0, 0);
+    
+    //endText1 = this.add.text(120, 400, 'Drops You Helped Collect: ', { fontFamily: 'Calibri', fontStyle:'bold', fontSize: '90px', fill: '#fff' });
+    endText2 = this.add.text(450, 600, total , { fontFamily: 'Calibri', fontStyle:'bold', fontSize: '90px', fill: '#fff' });
+    
+    
+    let playagainbutton = this.add.sprite(140, 1300, 'playagainbutton')
+    playagainbutton.setOrigin(0,0);
+    playagainbutton.setInteractive();
+    playagainbutton.on('pointerdown', () => this.scene.restart());
+    
+    let backtomenubutton = this.add.sprite(140, 1500, 'backtomenubutton')
+    backtomenubutton.setOrigin(0,0);
+    backtomenubutton.setInteractive();
+    //backtomenubutton.on('pointerdown', () => Game.shutUp());
+    backtomenubutton.on('pointerdown', () => this.scene.start(mainMenu));
+    
+    
+    total = 0;
+
+//    //restart game
+//    this.time.delayedCall(10000, function () {    
+//        this.scene.restart();
+//        mutr = false;
+//    }, [], this);
 }
 
 
+
+function resize() {
+    var canvas = game.canvas, width = window.innerWidth, height = window.innerHeight;
+    var wratio = width / height, ratio = canvas.width / canvas.height;
+
+    if (wratio < ratio) {
+        canvas.style.width = width + "px";
+        canvas.style.height = (width / ratio) + "px";
+    } else {
+        canvas.style.width = (height * ratio) + "px";
+        canvas.style.height = height + "px";
+    }
+}
  
 
 
@@ -267,7 +344,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 100 },
+            gravity: { y: 250 },
             debug: false
         }
     },
